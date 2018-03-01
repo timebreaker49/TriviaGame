95a433c684ed9cf51a9e0ffcf8dfd4767b3dff78
Wayne Baylor <wcsbaylor@gmail.com>
2018-02-28 22:43:19 -0600

    Generate questions and register clicks correctly

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,86 +11,81 @@ countdown();
             clearInterval(intervalId);
             alert("Time Up!");
             emptyQuestion();
+            index++;
             setTimeout(renderQuestion, 3000);
         }
+
     }
 
     intervalId = setInterval(countdown, 1000);
 
     function emptyQuestion() {
-    	$('.question').empty();
-    	$('#A').empty();
-    	$('#B').empty();
-    	$('#C').empty();
-    	$('#D').empty();
+    	$('#question').empty();
+    	$('#clickedAnswer').empty();
+
     }
 
     function renderQuestion() {
 
+       for (let i = 0; i < 1; i++) {
+            var questionPrompt = $('<h2>');
+            questionPrompt.addClass('#question');
+            // questionPrompt.attr('value', questionsArray[index].question[i]);
+            questionPrompt.text(questionsArray[index].question);
+            $('#question').append(questionPrompt);
+       } 
+
 //attempted loop
-    	// for (let i = 1; i < 5; i++) {
-	    // 	var qa = $('<button>');
-	    //     qa.attr('.col, .answer');
-	    //     qa.attr('value', questionsArray[i]);
-	    //     qa.text(questionsArray[i]);
-	    //     $('.answer').append(qa);
-    	// }
+    	for (let i = 0; i < 4; i++) {
+	    	var qa = $('<button>');
+	        qa.addClass('col, answer');
+            //value of the answer I"m populating, viewable in console
+	        qa.attr('value', questionsArray[index].answers[i]);
+            //.text is text to display
+	        qa.text(questionsArray[index].answers[i]);
+            //creates a value that can be linked to the .onClick button 
+            qa.attr('data-number', i);
+            $('#clickedAnswer').append(qa);
+	        // $('#answer-' + (i + 1)).append(qa);
+    	}
    
-        var answerA = $('<button>');
-        answerA.attr('.col, .answer');
-        answerA.attr('value', questionsArray[0].a);
-        answerA.text(questionsArray[0].a);
-        $('#A').append(answerA);
+        // questionsArray.splice(0, 1);
+        // console.log(questionsArray);
 
-        var answerB = $('<button>');
-        answerB.attr('.col, .answer');
-        answerB.attr('value', questionsArray[0].b);
-        answerB.text(questionsArray[0].b);
-        $('#B').append(answerB);
+    // runs the countdown function again
 
-        var answerC = $('<button>');
-        answerC.attr('.col, .answer');
-        answerC.attr('data-letter', questionsArray[0].c);
-        answerC.text(questionsArray[0].c);
-        $('#C').append(answerC);
-
-        var answerD = $('<button>');
-        answerD.attr('.col, .answer');
-        answerD.attr('value', questionsArray[0].d);
-        answerD.text(questionsArray[0].d);
-        $('#D').append(answerD);
-
-  
-        questionsArray.splice(0, 1);
-        console.log(questionsArray);
-
-    //runs the countdown function again
-        // number = 3;
-        // countdown();
-        // intervalId = setInterval(countdown, 1000);
+        number = 10;
+        countdown();
+        intervalId = setInterval(countdown, 1000);
     }
 
- $('.answer').on('click', function(event) {
-	var clickedChoice = $(this).data('letter');
+ $('#clickedAnswer').on('click', '.answer', function(event) { 
+	var clickedChoice = $(this).data('number');
 	console.log(clickedChoice);
-})    
+
+    console.log(questionsArray[index].correct);
+
+    if (clickedChoice + 1 === questionsArray[index].correct) {
+        alert("Correct!");
+    } else {
+        alert("Nice try, but no cigar");
+    }
+})  
+
+
+
+var index = -1;  
 
 var q1 = {
     question: "This anime features a young lad gathering Nakama on his way to becoming the Pirate King!",
-    a: "One Piece",
-    b: "Fullmetal Alchemist",
-    c: "Darker Than Black",
-    d: "Space Captain Yamato",
-    correct : "a",
+    answers: ["One Piece", "Fullmetal Alchemist", "Darker Than Black", "Space Captain Yamato"],
+    correct : 1
 }
 
 var q2 = {
-    question: "This comedy romance anime followed Captain Sosuke Sagara on his mission to protect high school student Kaname Chidori",
-    a: "Ichigo 100%",
-    b: "School Rumble",
-    c: "Fullmetal Panic",
-    d: "Fruits Basket",
-    correct : "c",
+    question: "In this comedy romance anime followed Captain Sosuke Sagara on his mission to protect high school student Kaname Chidori",
+    answers: ["Ichigo 100%", "School Rumble", "Fullmetal Panic", "Fruits Basket"],
+    correct : 2
 }
 
 var q3 = {
@@ -102,7 +97,7 @@ var q3 = {
     correct : "c",
 }
 
-var questionsArray = [q1, q2, q3];
+var questionsArray = [q1, q2];
 var number = 3;
 var intervalId;
 
